662eebe60fb4f02f01fd987027d7942014e1d80d
Devyansh Chaudhary <devyansh22156@iiitd.ac.in>
2024-03-31 00:03:39 +0530

    Added Customer HomePage and new User Profile section

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 import datetime
+import ast
+
+userMain = None
 
 app = Flask(__name__)
 
@@ -44,7 +47,27 @@ def signup():
     else:
         return render_template('/signup.html')
 
+def fetch_user_data(user_id):
+    connect = mysql.connector.connect(**db_config)
+    cursor = connect.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM customer WHERE CustomerID = %s", (user_id,))
+    user = cursor.fetchone()
+    cursor.close()
+    connect.close()
+    return user
 
+@app.route('/customerHome.html')
+def customerHome():
+    return render_template('customerHome.html')
+
+@app.route('/customer_profile.html')
+def customer_profile():
+    # Retrieve the user_id from the URL
+    user_id_str = request.args.get('user_id')
+    # Parse the user_id string into a dictionary
+    user_id = ast.literal_eval(user_id_str)
+    # Render the template with the user_id dictionary
+    return render_template('customer_profile.html', user=user_id)
 @app.route('/login.html', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
@@ -52,15 +75,16 @@ def login():
         password = request.form.get('password')
         if not email or not password:
             return "Email and password are required."
-        conn = mysql.connector.connect(**db_config)
-        cursor = conn.cursor(dictionary=True)
+        connect = mysql.connector.connect(**db_config)
+        cursor = connect.cursor(dictionary=True)
         cursor.execute("SELECT * FROM customer WHERE Email = %s AND Password = %s", (email, password))
         user = cursor.fetchone()
         cursor.close()
-        conn.close()
-
+        connect.close()
         if user:
-            return render_template('/customer_profile.html', user=user)
+            # Need to insert Main Home page Here
+            # return render_template('/customer_profile.html', user=user)
+            return render_template('/customerHome.html', user=user)
         else:
             return "Invalid email or password"
     else:
