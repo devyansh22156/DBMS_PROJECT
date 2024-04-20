6a1e0e16d5f1a5af690c070a0fd572b8292d24d8
Devyansh Chaudhary <devyansh22156@iiitd.ac.in>
2024-04-20 18:48:26 +0530

    log in fixed

--- kads.py
+++ kads.py
@@ -69,6 +69,9 @@ def main():
                 print("Logged in successfully!")
         elif choice == '3':
             print("Exiting...")
+
+
+
             break
         else:
             print("Invalid choice. Please try again.")
